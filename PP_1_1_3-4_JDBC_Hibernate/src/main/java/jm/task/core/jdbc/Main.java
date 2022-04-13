27ad3c8d8a394826a9de40b38cb0a5cf4cad0f2a
suwakoryukyu <hisoutensoku@suwako.ryukyu>
2022-04-13 17:13:49 +0500

    Created class Cat by class HelloWorld example

--- PP_1_1_3-4_JDBC_Hibernate/src/main/java/jm/task/core/jdbc/Main.java
+++ PP_1_1_3-4_JDBC_Hibernate/src/main/java/jm/task/core/jdbc/Main.java
@@ -7,6 +7,7 @@ import jm.task.core.jdbc.util.Util;
 import java.sql.SQLException;
 
 public class Main {
+
     public static void main(String[] args) throws SQLException {
         Util.getConnection();
         UserDao userDao = new UserDaoJDBCImpl();
